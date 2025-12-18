82e0748566675b8a9d1bb586b9ce5970f4ba65d7
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2025-12-18 14:02:38 +0200

    Added WatingDialogController and corresponding FXML for waiting dialog UI

---
+++ src/main/java/com/mycompany/tictactoeclient/WatingDialogController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.tictactoeclient;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+/**
+ * FXML Controller class
+ *
+ * @author siam
+ */
+public class WatingDialogController implements Initializable {
+
+
+    @FXML
+    private Button exitBtn;
+    @FXML
+    private Label name;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void exitAction(ActionEvent event) {
+    }
+
+}
